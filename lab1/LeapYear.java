52b1d7b3647567063b7e7847d90ac8a5c01b7eec
ljy <123@123.com>
2024-08-04 15:19:23 +0800

    test for idea

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -8,7 +8,9 @@ public class LeapYear {
      */
     private static void isLeapYear(int year)
     {
-return true;	    
+        if(year%400==0)return true;
+        if(year%4==0&&year%100!=0)return true;
+        return false;
     }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
